533afab8968dcfb0ba0fe5ed1b30e0d099938737
leroywan <leroywan@gmail.com>
2017-03-28 21:31:29 -0400

    created general features

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,17 +1,80 @@
 'use strict';
 
-angular.module('medicationReminderApp').controller('MainCtrl', function ($scope, $http, $window) {
+angular.module('medicationReminderApp')
+	.filter('formatTime', function() {
+		return function(input, twentyFour) {
+			input = input || '';
+			var output = moment(input).format('LT');
+	
+			// Conditional for a 24 hour clock
+			if (twentyFour) {
+				output = moment(input).format('HH:mm');
+			}
 
-    var start = moment().format('MM/DD/YYYY'),
-        end = moment().add(1, 'day').format('MM/DD/YYYY');
+			return output;
+		}
+	})
+	.filter('getDate', function() {
+		return function(input) {
+			input = input || "";
+			var output = moment.utc(input).format('MMMM Do');
 
-    $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
-        $scope.meds = meds.data;
-    });
+			return output
+		}
+	})
+	.controller('MainCtrl', function ($scope, $http, $window) {
 
-    $window.setInterval(function () {
-        $scope.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-        $scope.$apply();
-    }, 1000);
+	    var start = moment().format('MM/DD/YYYY'),
+	        end = moment().add(1, 'day').format('MM/DD/YYYY');
 
-});
+	    $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
+	        $scope.meds = meds.data;
+	        setMissedMeds($scope.meds);
+	    });
+
+	    // initialize the time to prevent lag
+	    $scope.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+	    $window.setInterval(function () {
+	        $scope.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+	        $scope.$apply();
+	        checkMedTimeWindow($scope.meds);
+	    }, 1000);
+
+	    // check if the medication is past the current time and create property
+	    var setMissedMeds = function(meds) {
+	        for (var i=0; i<meds.length; i++) {
+	        	var currentTime = moment();
+	        	// 5 minute grace period before med is considered missed
+	        	var medTime = moment(meds[i].time).add(5, 'minutes');
+	        	
+	        	if (currentTime.isBefore(medTime)) {
+	        		meds[i].missed = false;
+	        	} else {
+	        		meds[i].missed = true;
+	        	}
+	        }
+	    }
+
+	    var checkMedTimeWindow = function(meds) {
+	    	for (var i=0; i<meds.length; i++) {
+	    		var deltaTime = moment.duration("00:05:00");
+	    		var medTimeBegin = moment(meds[i].time).subtract(5, 'minutes');
+	    		var medTimeEnd = moment(meds[i].time).add(5, 'minutes');
+
+	    		if (moment().isBetween(medTimeBegin, medTimeEnd)){
+	    			meds[i].openTimeWindow = true;
+	    		} else {
+	    			meds[i].openTimeWindow = false;
+	    		}
+	    	}
+	    }
+
+	    // Preset format to 12 hours and create toggle to display 24 hours
+	    $scope.twentyFour = false;
+	    $scope.toggleFormat = function() {
+	    	console.log($scope.meds);
+	    	$scope.twentyFour = ($scope.twentyFour == true ? false : true);
+	    	return 
+	    }
+
+	});
--- server/config/seed.js
+++ server/config/seed.js
@@ -13,25 +13,93 @@ q(Medication.findOne({}).exec()).then(function (med) {
     var currentDate = moment().startOf('day'),
         numMeds,
         i;
-    while (!med && currentDate.isBefore(moment().add(1, 'year'))) {
-        numMeds = Math.floor((Math.random() * 24) + 1);
-        for (i = 1; i <= numMeds; i++) {
-            Medication.create({
-                name: 'Medication ' + i,
-                dosage: i * 10 + ' mL',
-                time: currentDate.clone().add(i * 24 / numMeds, 'hours').toDate(),
-                completed: false,
-                d: {
-                    c: moment().toDate()
-                }
-            }, function (err, result) {
-                if (err) {
-                    console.error(err);
-                }
-            });
+        
+    Medication.remove({ }, function(err, result) {if (err) throw err});
+    Medication.create({
+        name: 'New Medicine',
+        dosage: 10 + ' mL',
+        time: moment().add(2, 'hours').toDate(),
+        completed: false,
+        d: {
+            c: moment().toDate()
         }
-        currentDate.add(1, 'day')
-    }
+    }, function (err, result) {
+        if (err) {
+            console.error(err);
+        }
+    });
+    Medication.create({
+        name: 'Missed Medicine',
+        dosage: 10 + ' mL',
+        time: moment().subtract(2, 'hours').toDate(),
+        completed: false,
+        d: {
+            c: moment().toDate()
+        }
+    }, function (err, result) {
+        if (err) {
+            console.error(err);
+        }
+    });
+    Medication.create({
+        name: 'Pending Medicine',
+        dosage: 20 + ' mL',
+        time: moment().subtract(3, 'minutes').toDate(),
+        completed: false,
+        d: {
+            c: moment().toDate()
+        }
+    }, function (err, result) {
+        if (err) {
+            console.error(err);
+        }
+    });
+    Medication.create({
+        name: 'Pending Medicine',
+        dosage: 20 + ' mL',
+        time: moment().add(3, 'minutes').toDate(),
+        completed: false,
+        d: {
+            c: moment().toDate()
+        }
+    }, function (err, result) {
+        if (err) {
+            console.error(err);
+        }
+    });
+     Medication.create({
+        name: 'Completed Medicine',
+        dosage: 20 + ' mL',
+        time: moment().subtract(1, 'hours').toDate(),
+        completed: false,
+        d: {
+            c: moment().toDate()
+        }
+    }, function (err, result) {
+        if (err) {
+            console.error(err);
+        }
+    });
+
+    // while (!med && currentDate.isBefore(moment().add(1, 'year'))) {
+    //     numMeds = Math.floor((Math.random() * 24) + 1);
+    //     for (i = 1; i <= numMeds; i++) {
+    //         Medication.create({
+    //             name: 'Medication ' + i,
+    //             dosage: i * 10 + ' mL',
+    //             time: currentDate.clone().add(i * 24 / numMeds, 'hours').toDate(),
+    //             completed: false,
+    //             d: {
+    //                 c: moment().toDate()
+    //             }
+    //         }, function (err, result) {
+    //             if (err) {
+    //                 console.error(err);
+    //             }
+    //         });
+    //     }
+    //     currentDate.add(1, 'day')
+    // }
 }).catch(function (err) {
     console.log('Error running seed', err);
 });
