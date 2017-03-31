9ac7ead78a66bbce442106c4dbdf3475be3bb4d7
leroywan <leroywan@gmail.com>
2017-03-31 11:35:12 -0400

    edited the database to refresh everytime the server is loaded

--- server/config/seed.js
+++ server/config/seed.js
@@ -9,10 +9,10 @@ var moment = require('moment'),
     q = require('q'),
     Medication = require('../api/medication/medication.model');
 
-q(Medication.findOne({}).exec()).then(function (med) {
-    var currentDate = moment().startOf('day'),
-        numMeds,
-        i;
+// q(Medication.findOne({}).exec()).then(function (med) {
+//     var currentDate = moment().startOf('day'),
+//         numMeds,
+//         i;
 
     Medication.remove({ }, function(err, result) {if (err) throw err});
     Medication.create({
@@ -97,25 +97,25 @@ q(Medication.findOne({}).exec()).then(function (med) {
 
 
 
-    // while (!med && currentDate.isBefore(moment().add(1, 'year'))) {
-    //     numMeds = Math.floor((Math.random() * 24) + 1);
-    //     for (i = 1; i <= numMeds; i++) {
-    //         Medication.create({
-    //             name: 'Medication ' + i,
-    //             dosage: i * 10 + ' mL',
-    //             time: currentDate.clone().add(i * 24 / numMeds, 'hours').toDate(),
-    //             completed: false,
-    //             d: {
-    //                 c: moment().toDate()
-    //             }
-    //         }, function (err, result) {
-    //             if (err) {
-    //                 console.error(err);
-    //             }
-    //         });
-    //     }
-    //     currentDate.add(1, 'day')
-    // }
-}).catch(function (err) {
-    console.log('Error running seed', err);
-});
+//     while (!med && currentDate.isBefore(moment().add(1, 'year'))) {
+//         numMeds = Math.floor((Math.random() * 24) + 1);
+//         for (i = 1; i <= numMeds; i++) {
+//             Medication.create({
+//                 name: 'Medication ' + i,
+//                 dosage: i * 10 + ' mL',
+//                 time: currentDate.clone().add(i * 24 / numMeds, 'hours').toDate(),
+//                 completed: false,
+//                 d: {
+//                     c: moment().toDate()
+//                 }
+//             }, function (err, result) {
+//                 if (err) {
+//                     console.error(err);
+//                 }
+//             });
+//         }
+//         currentDate.add(1, 'day')
+//     }
+// }).catch(function (err) {
+//     console.log('Error running seed', err);
+// });
